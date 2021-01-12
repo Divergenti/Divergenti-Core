1b05a4634dd60bec09993b5f4f12a85c5b3a0ef4
checho1989 <cheverdolaga@hotmail.com>
2021-01-11 23:39:23 -0500

    Regex create new wallet

--- src/app/components/account/create/create.component.ts
+++ src/app/components/account/create/create.component.ts
@@ -75,8 +75,6 @@ export class CreateAccountComponent implements OnInit {
             accountName: new FormControl('', Validators.compose([
                 Validators.required,
                 Validators.minLength(1),
-                Validators.maxLength(24),
-                Validators.pattern(/^[a-zA-Z0-9]*$/),
                 Validators.maxLength(42),
                 Validators.pattern(/^[a-zA-Z0-9-_.]*$/)
             ]))
@@ -167,7 +165,7 @@ export class CreateAccountComponent implements OnInit {
                     chainCode: masterNode.chainCode,
                     network: 'DivergentiMain',
                     creationTime: Date.now() / 1000,
-                    coinType: 1926,
+                    coinType: 596,
                     lastBlockSyncedHeight: 0,
                     lastBlockSyncedHash: ''
                 });
